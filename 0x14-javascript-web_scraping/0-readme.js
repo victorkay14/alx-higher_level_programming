df52d4c46efd72870248d0b82e35e10e9bf7be5e
victorkay14 <jvictorkingorikay@gmail.com>
2024-01-04 18:27:07 -0800

    a script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 3) {
-  console.error('Usage: node read-file.js <file-path>');
+  console.error('Usage: ./read-file.js <file-path>');
   process.exit(1);
 }
 
@@ -18,6 +18,7 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     // Print the error if reading the file fails
     console.error(`Error reading file: ${err.message}`);
+    process.exit(1);
   } else {
     // Print the content of the file if reading is successful
     console.log('File content:\n', data);
